1c2d4fcb65b57a82518b0a8a8daec1b8df507a27
anis <anishammouche50@gmail.com>
2024-03-05 23:35:56 +0000

    amelioration du text et link btm

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import confetti from "canvas-confetti";
 import "./style/init.css"
 import "./foot"
+import "https://kit.fontawesome.com/4c536a6bd5.js"
 
+
+
+const btn=document.querySelector(".button");
+btn.addEventListener("click",()=>{
+    window.location.href="../public/menu.html"
+});
 // import './page/anis.svg'
 confetti();
