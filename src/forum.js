91ab69db1e378e6d87beca7b073604bbd510e219
anis <anishammouche50@gmail.com>
2024-03-06 22:58:22 +0000

    power

---
+++ src/forum.js
@@ -0,0 +1,8 @@
+import "./style/init.css";
+import "./style/forum.css";
+import confetti from "canvas-confetti";
+
+import "./foot";
+import "https://kit.fontawesome.com/4c536a6bd5.js";
+
+confetti();
