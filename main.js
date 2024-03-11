89bf408e236e1f25a1b2135e551ce69204778849
anis <anishammouche50@gmail.com>
2024-03-11 14:35:46 +0000

    oui

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import confetti from "canvas-confetti";
-import "./style/init.css";
-import "./foot";
+import "./src/style/init.css";
+import "./src/foot";
 import "https://kit.fontawesome.com/4c536a6bd5.js";
-import "./style/index.css";
+import "./src/style/index.css";
 
 // var sidenav = document.getElementById("mySidenav");
 // var openBtn = document.getElementById("openBtn");
