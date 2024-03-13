3439d6b2837e17b4c9aabeae167d03c53ed10590
anis <anishammouche50@gmail.com>
2024-03-13 22:27:54 +0000

    Updates

--- src/about.js
+++
@@ -1,8 +0,0 @@
-import "./style/about.css";
-import confetti from "canvas-confetti";
-import "./style/style.css";
-import "./foot";
-import "https://kit.fontawesome.com/4c536a6bd5.js";
-document.querySelector("img").src = "./photo/ico.png";
-
-confetti();
--- src/forum.js
+++
@@ -1,9 +0,0 @@
-import "./style/style.css";
-import "./style/forum.css";
-import confetti from "canvas-confetti";
-
-import "./foot";
-import "https://kit.fontawesome.com/4c536a6bd5.js";
-document.querySelector("img").src = "./photo/ico.png";
-
-confetti();
